c6838aead0298d4e84eab2babaf092282ec7cc2a
pichurin_roman <pichurin.roman@gmail.com>
2018-12-04 23:24:46 +0200

    BC- Delete old project, update gitignore file

--- BooksDBProject/BooksDBProject/dbo/Tables/Book.sql
+++
@@ -1,8 +0,0 @@
-﻿CREATE TABLE [dbo].[Book]
-(
-	[Id] INT NOT NULL PRIMARY KEY,
-	[Title]	NVARCHAR(100) NOT NULL,
-	[Pages] INT	CHECK(Pages > 0) NOT NULL,
-	[Rating] FLOAT CHECK(Rating >= 0 AND Rating <= 1) NOT NULL DEFAULT(0),
-	[DateOfPublication] DATE NOT NULL,
-)
