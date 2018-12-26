acda2f9bd945ee496a89d26f49e1569d046fe200
pichurin_roman <pichurin.roman@gmail.com>
2018-12-27 01:06:16 +0200

    DB-Add first SP

---
+++ BooksCatalogue/DatabaseProject/dbo/SP/USP_Get_Author.sql
@@ -0,0 +1,13 @@
+﻿CREATE PROCEDURE [dbo].[USP_Get_Author]
+	@id INT
+AS 
+	SELECT a.[Id], a.[FirstName], a.[LastName],
+	b.[Id], b.[Title], b.[Pages], b.[Rating],b.[DateOfPublication]	
+
+	FROM [Authors] AS a
+	LEFT JOIN [AuthorBook] AS ab ON ab.[AuthorId] = a.[Id]
+	LEFT JOIN [Books] AS b on b.[Id] = b.[Id]
+	WHERE [a].[Id] = @Id
+	ORDER BY b.[Title]
+
+RETURN 0
